7bd87cd5de6f9d0ad55bdeee59ddbe79fa5606af
Brad Thome <brad.thome@bottlerocketstudios.com>
2017-07-28 12:46:34 -0500

    Good stuff

---
+++ Local WKWebView Server/Local WKWebView Server.h
@@ -0,0 +1,19 @@
+//
+//  Local WKWebView Server.h
+//  Local WKWebView Server
+//
+//  Created by Bradley Thome on 7/28/17.
+//  Copyright © 2017 Bradley Thome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Local WKWebView Server.
+FOUNDATION_EXPORT double Local_WKWebView_ServerVersionNumber;
+
+//! Project version string for Local WKWebView Server.
+FOUNDATION_EXPORT const unsigned char Local_WKWebView_ServerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Local_WKWebView_Server/PublicHeader.h>
+
+
